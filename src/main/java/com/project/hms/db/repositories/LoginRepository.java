4f4e1b751394a8f152252250b52db6c06f8ccbd5
Aiswaryadhan <aisu1495@gmail.com>
2020-06-28 10:38:52 +0530

    Added query for insertion in userstore

--- src/main/java/com/project/hms/db/repositories/LoginRepository.java
+++ src/main/java/com/project/hms/db/repositories/LoginRepository.java
@@ -1,7 +1,16 @@
 package com.project.hms.db.repositories;
 
 import com.project.hms.db.models.Login;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface LoginRepository extends CrudRepository<Login,String> {
+    @Transactional
+    @Modifying
+    @Query(
+            value = "insert into userstore values(:loginId,:loginId,NOW())",
+            nativeQuery = true)
+    void insertUserStore(String loginId);
 }
